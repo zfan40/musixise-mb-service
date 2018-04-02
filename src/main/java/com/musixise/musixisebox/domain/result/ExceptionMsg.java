05a95a98c9cff588225daf0d073aa65537d40479
zhaowei <zhaowei.me@gmail.com>
2018-04-02 23:48:06 +0800

    修改文案

--- src/main/java/com/musixise/musixisebox/domain/result/ExceptionMsg.java
+++ src/main/java/com/musixise/musixisebox/domain/result/ExceptionMsg.java
@@ -4,10 +4,10 @@ package com.musixise.musixisebox.domain.result;
  * Created by zhaowei on 2018/4/1.
  */
 public enum  ExceptionMsg {
-    SUCCESS("0", "操作成功"),
-    FAILED("20000","操作失败"),
+    SUCCESS("0", "sucess"),
+    FAILED("20000","fail"),
     NEED_LOGIN("40000","请登录"),
-    ParamError("20001", "参数错误！");
+    ParamError("20001", "参数错误");
 
     private ExceptionMsg(String code, String msg) {
         this.code = code;
