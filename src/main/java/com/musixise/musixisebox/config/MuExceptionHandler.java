576ef727c17c0ac71a80371d0b93ef550650806c
zhaowei <zhaowei.me@gmail.com>
2018-04-05 23:42:40 +0800

    处理参数绑定信息

--- src/main/java/com/musixise/musixisebox/config/MuExceptionHandler.java
+++ src/main/java/com/musixise/musixisebox/config/MuExceptionHandler.java
@@ -7,10 +7,14 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by zhaowei on 2018/4/5.
@@ -64,5 +68,19 @@ public class MuExceptionHandler implements Ordered {
 
         logger.info("请求地址：" + request.getRequestURL());
         return new MusixiseResponse(ExceptionMsg.PARAM_ERROR, e.getMessage());
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public MusixiseResponse processValidationError(BindException ex) {
+        logger.error(ex.getMessage(), ex);
+        String result = ex
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(","));
+
+        return new MusixiseResponse(ExceptionMsg.PARAM_ERROR, result);
     }
 }
