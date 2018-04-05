32c46a5f2cf631ac6fde84f6078423e403f96168
zhaowei <zhaowei.me@gmail.com>
2018-04-05 21:38:23 +0800

    处理参数异常

--- src/main/java/com/musixise/musixisebox/config/MuExceptionHandler.java
+++ src/main/java/com/musixise/musixisebox/config/MuExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.musixise.musixisebox.config;
 
 import com.musixise.musixisebox.MusixiseException;
+import com.musixise.musixisebox.domain.result.ExceptionMsg;
 import com.musixise.musixisebox.domain.result.ResponseData;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * Created by zhaowei on 2018/4/5.
@@ -40,14 +41,15 @@ public class MuExceptionHandler implements Ordered {
 
     /**
      * 处理所有接口数据验证异常
-     * @param e
+     * @param e MethodArgumentTypeMismatchException
      * @return
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
     @ResponseBody
-    ResponseData handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+    ResponseData handleMethodArgumentNotValidException(MethodArgumentTypeMismatchException e,
+                                                       HttpServletRequest request){
 
         logger.info("请求地址：" + request.getRequestURL());
-        return ResponseData.errorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return new ResponseData(ExceptionMsg.PARAM_ERROR, e.getMessage());
     }
 }
