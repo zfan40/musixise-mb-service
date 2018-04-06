99900835507f41828ff68ae1ad6351a5a37e5be5
zhaowei <zhaowei.me@gmail.com>
2018-04-06 11:20:00 +0800

    引入 Swagger2

---
+++ src/main/java/com/musixise/musixisebox/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.musixise.musixisebox.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by zhaowei on 2018/4/6.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return  new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.musixise.musixisebox"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("musixise.com RESTful APIs")
+                .description("spring boot , swagger2")
+                .termsOfServiceUrl("github-url")
+                .contact("musixise.com")
+                .version("1.0")
+                .build();
+    }
+}
