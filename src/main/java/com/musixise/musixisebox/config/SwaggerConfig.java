1797954fca635b570d18936770e106ab3b871d93
zhaowei <zhaowei.me@gmail.com>
2018-04-06 14:45:08 +0800

    修改文档描述

--- src/main/java/com/musixise/musixisebox/config/SwaggerConfig.java
+++ src/main/java/com/musixise/musixisebox/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,11 +28,11 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
-                .title("musixise.com RESTful APIs")
+                .title("MUSIXISE RESTful APIs")
                 .description("spring boot , swagger2")
                 .termsOfServiceUrl("github-url")
-                .contact("musixise.com")
-                .version("1.0")
+                .contact(new Contact("zhaowei, zfan40", "www.musixise.com", "developer@musixise.com"))
+                .version("1.0.0")
                 .build();
     }
 }
