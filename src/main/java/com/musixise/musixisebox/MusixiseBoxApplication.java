64834693f2f246f3838fa738a4538299f4e2828f
zhaowei <zhaowei.me@gmail.com>
2018-04-07 12:42:43 +0800

    处理 404

--- src/main/java/com/musixise/musixisebox/MusixiseBoxApplication.java
+++ src/main/java/com/musixise/musixisebox/MusixiseBoxApplication.java
@@ -1,6 +1,7 @@
 package com.musixise.musixisebox;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @ComponentScan
+@EnableAutoConfiguration
 public class MusixiseBoxApplication {
 
 	public static void main(String[] args) {
