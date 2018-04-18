75e988f57fa06c6b70870be9f1127dd02aec44c8
zhaowei <zhaowei.me@gmail.com>
2018-04-18 22:27:08 +0800

    add cache

--- src/main/java/com/musixise/musixisebox/MusixiseBoxApplication.java
+++ src/main/java/com/musixise/musixisebox/MusixiseBoxApplication.java
@@ -3,11 +3,15 @@ package com.musixise.musixisebox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
+@EnableCaching
 @ComponentScan
 @EnableAutoConfiguration
 public class MusixiseBoxApplication {
