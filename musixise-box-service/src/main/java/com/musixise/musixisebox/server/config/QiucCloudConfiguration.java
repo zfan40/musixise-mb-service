ca51a56d1e980eae862bcdc240c3b040c59d7dc5
zhaowei <zhaowei.me@gmail.com>
2018-12-02 20:23:21 +0800

    修改配置

--- musixise-box-service/src/main/java/com/musixise/musixisebox/server/config/QiucCloudConfiguration.java
+++ musixise-box-service/src/main/java/com/musixise/musixisebox/server/config/QiucCloudConfiguration.java
@@ -5,6 +5,7 @@ import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,13 +16,8 @@ import org.springframework.context.annotation.Configuration;
 public class QiucCloudConfiguration {
     private final Logger log = LoggerFactory.getLogger(QiucCloudConfiguration.class);
 
-    //设置好账号的ACCESS_KEY和SECRET_KEY
-    String ACCESS_KEY = "ojwu8Dif9IIu2dh_9BAyr_VA1aG7kIxCFkoH6n-r";
-    String SECRET_KEY = "xdV7nuNU-M7Bw8d7qorRaxK3yENNS9Z7obvKGlIg";
-    //要上传的空间
-
-    //密钥配置
-
+    @Value("${spring.qiniu.accessKey}") String ACCESS_KEY;
+    @Value("${spring.qiniu.secretKey}") String SECRET_KEY;
     //创建上传对象
 
     @Bean
