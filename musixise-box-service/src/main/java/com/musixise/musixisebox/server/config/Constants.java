2bf9ec8486b32c00f3a5e2b8ea3a78730ec23d00
zhaowei <zhaowei.me@gmail.com>
2018-12-04 22:24:23 +0800

    地址修改

--- musixise-box-service/src/main/java/com/musixise/musixisebox/server/config/Constants.java
+++ musixise-box-service/src/main/java/com/musixise/musixisebox/server/config/Constants.java
@@ -5,6 +5,6 @@ package com.musixise.musixisebox.server.config;
  */
 public class Constants {
 
-    public static final String QINIU_IMG_DOMAIN = "http://img.musixise.com/%s";
-    public static final String QINIU_AUDIO_DOMAIN = "http://audio.musixise.com/%s";
+    public static final String QINIU_IMG_DOMAIN = "//img.musixise.com/%s";
+    public static final String QINIU_AUDIO_DOMAIN = "//audio.musixise.com/%s";
 }
--- musixise-box-service/src/main/java/com/musixise/musixisebox/server/utils/FileUtil.java
+++ musixise-box-service/src/main/java/com/musixise/musixisebox/server/utils/FileUtil.java
@@ -32,7 +32,7 @@ public class FileUtil {
                 }
             }
         }
-        return fileName;
+        return fileName.replaceAll("https?:", "");
     }
 
     public static String getImageFullName(String name) {
