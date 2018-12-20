0cf2676dda3fb26777f539451a3b015987ff67b9
zhaowei <zhaowei.me@gmail.com>
2018-12-20 22:59:15 +0800

    add QueryDSL

--- musixise-box-shop/src/main/kotlin/com/musixise/musixisebox/shop/repository/OrderRepository.kt
+++ musixise-box-shop/src/main/kotlin/com/musixise/musixisebox/shop/repository/OrderRepository.kt
@@ -2,5 +2,6 @@ package com.musixise.musixisebox.shop.repository
 
 import com.musixise.musixisebox.shop.domain.Order
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long>,QuerydslPredicateExecutor<Order>
