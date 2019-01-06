d9338870e8446f1fcbe38bfa15664e4214674d8b
zhaowei <zhaowei.me@gmail.com>
2019-01-06 22:22:18 +0800

    generate orderid

---
+++ musixise-box-shop/src/main/kotlin/com/musixise/musixisebox/shop/utils/OrderUtil.kt
@@ -0,0 +1,37 @@
+package com.musixise.musixisebox.shop.utils
+
+import org.apache.commons.lang.StringUtils
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+object OrderUtil {
+
+    /**
+     * 生成订单号
+     * 20190106222026003b9ac9ff02540be3ff
+     */
+    fun genOrderId(userId: Long, orderId: Long): String {
+
+        val sdf = SimpleDateFormat("yyyyMMddHHmmss")
+        val format = sdf.format(Date())
+        val userIdHex  = StringUtils.leftPad(java.lang.Long.toHexString(userId), 10, "0")
+        val orderIdHex = StringUtils.leftPad(java.lang.Long.toHexString(orderId), 10, "0")
+        return "${format}${userIdHex}${orderIdHex}"
+    }
+
+    /**
+     * 解析订单号
+     */
+    fun getOrderId(orderId: String): Long {
+        return java.lang.Long.parseLong(orderId.substring(24).replaceFirst("^0*", ""), 16)
+    }
+
+    /**
+     * 解析出用户ID
+     */
+    fun getUserId(orderId: String): Long {
+        return java.lang.Long.parseLong(orderId.substring(14, 24).replaceFirst("^0*", ""), 16)
+    }
+
+}
