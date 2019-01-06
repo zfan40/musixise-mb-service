0b03dd5662f45407988886e64243c60e73b02ccb
zhaowei <zhaowei.me@gmail.com>
2019-01-06 15:32:26 +0800

    add ip

---
+++ musixise-box-service/src/main/java/com/musixise/musixisebox/server/config/IPLogConfig.java
@@ -0,0 +1,20 @@
+package com.musixise.musixisebox.server.config;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPLogConfig extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
